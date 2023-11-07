cb0a2cd6d3af02fcb1c38d26ab023c645cae676b
AlexanderHHansen <61501072+AlexanderHHansen@users.noreply.github.com>
2023-11-07 13:46:53 +0100

    more work on conversion

--- graph_builder/model_constructor.py
+++ graph_builder/model_constructor.py
@@ -10,18 +10,25 @@ from config.Config import Config
 
 
 class ModelConstructor:
+    """Loads and convert the partquet files to objects. 
+    """
+
     def __init__(self) -> None:
         self.entities: dict[str, Entity] = {}
         self.relations: dict[str, Relation] = {}
         self.features: dict[str, Feature] = {}
 
     def build(self):
+        """Loads every parquet file and converts them to entity, relation and feature objects. 
+        """
         self.construct_entities()
         self.construct_features()
         self.construct_relations()
         self.combine_patients()
 
     def combine_patients(self):
+        """Combine the both the patients with and without sepsis. 
+        """
         patients = self.entities["patient"]
         sepsis_patients = self.entities["sepsis_cohort"]
         patients.combine_entity(sepsis_patients)
@@ -71,31 +78,24 @@ class ModelConstructor:
             self.relations[file_name] = new_relation
 
     def construct_features(self):
-        for rel in Config.features:
+        for feat in Config.features:
             # Read relation information
-            file_name = rel["file_name"]
-            relation_name = rel["relation_name"]
-            direction = Direction(rel["direction"])
-            sub = rel["sub"]
-            obj = rel["obj"]
-
-            entity1 = self.entities[sub]
-            entity2 = self.entities[obj]
+            name = feat["file_name"]
+            sub = feat["sub"]
+            entity = self.entities[sub]
 
             # Read parquet file with entity ids
             df = self.safe_read(
-                Path(Config.output_folder) / f"{file_name}.parquet", file_name
+                Path(Config.output_folder) / f"{name}.parquet", name
             )
 
             # Construct new Relation
-            new_relation = Relation(
-                sub=entity1,
-                obj=entity2,
-                relation_name=relation_name,
+            new_feature = Feature(
+                sub=entity,
                 mapping=df,
             )
 
-            self.relations[file_name] = new_relation
+            self.features[name] = new_feature
 
     def safe_read(self, file_path: Path, name: str) -> Optional[pd.DataFrame]:
         if os.path.exists(file_path):
--- graph_builder/models/feature.py
+++ graph_builder/models/feature.py
@@ -1,2 +1,42 @@
+import pandas as pd 
+from models.entity import Entity
+
 class Feature:
-    pass
+    def __init__(
+        self,
+        sub: Entity,
+        mapping: pd.DataFrame,
+    ):
+        self.sub = sub
+        self.mapping = self.construct_mapping(mapping)
+        self.features_names = self.get_feature_names(mapping)
+
+    def construct_mapping(self, mapping: pd.DataFrame) -> pd.DataFrame:
+        """Removes duplicates from mapping.
+
+        Args:
+            mapping (pd.DataFrame): dataframe that maps entity to its features.
+            each row has an id equal to the entity, rest of the columns are features. 
+            ex. columns of mapping: ["patient", "patient_feature1", "patient_feature2", ...]
+
+        Returns:
+            pd.DataFrame: mapping without duplicates
+        """
+
+        # Filter mappings with non-existing entity ids
+        mapping = mapping[mapping[self.sub.name].isin(self.sub.ids)]
+
+        return mapping.drop_duplicates()
+
+    def get_feature_names(self, mapping: pd.DataFrame) -> list[str]:
+        """Returns a list of names for the features in the mapping. 
+
+        Args:
+            mapping (pd.DataFrame): mapping df
+
+        Returns:
+            list[str]: list of feature names, ie. ["feature1", "feature2", ...]
+        """
+        cols = list(mapping.columns)
+        cols.remove(self.sub.name)
+        return cols 
