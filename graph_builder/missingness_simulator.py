9a180ccebd1b74e4f1f76f28cf12bc7e60df5bf8
AlexanderHHansen <61501072+AlexanderHHansen@users.noreply.github.com>
2024-02-05 14:22:42 +0100

    doc string

--- graph_builder/missingness_simulator.py
+++ graph_builder/missingness_simulator.py
@@ -3,8 +3,20 @@ import numpy as np
 import pandas as pd
 
 class MissingSimulator:
+    """Class for simulating missingness in data.
+    """
         
     def simulate_attribute_mcar(self, attributes: pd.DataFrame, missingness_rate: float) -> pd.DataFrame:
+        """Simulate MCAR missingness of node attributes.
+
+        Args:
+            attributes (pd.DataFrame): Attributes to simulate missingness on.
+            missingness_rate (float): The rate of missingness to simulate.
+                0.01 is 1% missingness, 0.1 is 10% missingness, etc.
+
+        Returns:
+            pd.DataFrame: A new dataframe with missingness simulated.
+        """
         attributes = attributes.copy(deep=True)
         mask = np.random.uniform(0, 1, size=attributes.shape) <= missingness_rate
         attributes[mask] = np.nan
--- tests/graph_builder/missingness_simulator_test.py
+++ tests/graph_builder/missingness_simulator_test.py
@@ -9,7 +9,7 @@ from graph_builder.missingness_simulator import MissingSimulator
         (1,1), (3,3), (5,5), (7,7), (10,10), (15,15), (20,20), (25,25), (30,30), (40,40), (50,50), (60,60), (70,70), (80,80), (90,90), (100,100)
     ],
 )
-def test_simulate_attribute_mcar(missing_rate, expected):    
+def test_simulate_attribute_mcar_correct_missing_rate(missing_rate, expected):    
     np.random.seed(42)
     df = pd.DataFrame({
         'A': np.random.rand(10),
@@ -24,7 +24,7 @@ def test_simulate_attribute_mcar(missing_rate, expected):
         'J': np.random.rand(10),
     })
 
-    df = MissingSimulator().simulate_attribute_mcar(df, missing_rate / 100)
+    df = MissingSimulator().simulate_attribute_mcar(df, missing_rate / 100, np.nan)
     actual_missing_rate = df.isnull().sum().sum() / 100
     expected_rate = expected / 100
 
