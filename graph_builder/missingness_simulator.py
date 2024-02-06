16d6e22c74b07baf95af88c6e6c94c3e41b87e40
AlexanderHHansen <61501072+AlexanderHHansen@users.noreply.github.com>
2024-02-06 08:56:03 +0100

    extra param

--- graph_builder/missingness_simulator.py
+++ graph_builder/missingness_simulator.py
@@ -1,12 +1,17 @@
 from enum import Enum, auto
 import numpy as np
 import pandas as pd
+from typing import Any
 
 class MissingSimulator:
     """Class for simulating missingness in data.
     """
         
-    def simulate_attribute_mcar(self, attributes: pd.DataFrame, missingness_rate: float) -> pd.DataFrame:
+    def simulate_attribute_mcar(
+            self, attributes: pd.DataFrame, 
+            missingness_rate: float, 
+            missing_value: Any = np.nan
+    ) -> pd.DataFrame:
         """Simulate MCAR missingness of node attributes.
 
         Args:
@@ -19,5 +24,5 @@ class MissingSimulator:
         """
         attributes = attributes.copy(deep=True)
         mask = np.random.uniform(0, 1, size=attributes.shape) <= missingness_rate
-        attributes[mask] = np.nan
+        attributes[mask] = missing_value
         return attributes
